25b62d1ad65798833da0cbb518780947f5ac079e
handcraftsman <gar3ts@gmail.com>
2010-03-17 21:21:41 +0000

    move PropertyMappingInfo and SimpleMapper.GetAccessors() to Reflection in MvbaCore.dll update MvbaCore.dll

--- src/MvbaMapper/SimpleMapper.cs
+++ src/MvbaMapper/SimpleMapper.cs
@@ -1,43 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 using CodeQuery;
+
+using MvbaCore;
 
 namespace MvbaMapper
 {
 	public class SimpleMapper
 	{
-		public static IEnumerable<PropertyMappingInfo> GetAccessors(Type sourceType, Type destinationType)
-		{
-			var sourceProperties = sourceType
-				.GetProperties()
-				.ThatHaveAGetter()
-				.ToDictionary(x => x.Name.ToLower());
-			var destinationProperties = destinationType
-				.GetProperties()
-				.ThatHaveASetter()
-				.Where(x => sourceProperties.ContainsKey(x.Name.ToLower()))
-				.ToDictionary(x => x.Name.ToLower());
-			var accessors = new List<PropertyMappingInfo>();
-			foreach (var destinationProperty in destinationProperties)
-			{
-				var sourceProperty = sourceProperties[destinationProperty.Key];
-
-				var property = destinationProperty;
-				var propertyMappingInfo = new PropertyMappingInfo
-					{
-						Name = destinationProperty.Value.Name,
-						SourcePropertyType = sourceProperty.PropertyType,
-						DestinationPropertyType = destinationProperty.Value.PropertyType,
-						GetValueFromSource = source => sourceProperty.GetValue(source, null),
-						SetValueToDestination = (destination, value) => property.Value.SetValue(destination, value, null)
-					};
-				accessors.Add(propertyMappingInfo);
-			}
-			return accessors;
-		}
-
 		public void Map(object source, object destination)
 		{
 			if (source == null)
@@ -48,7 +19,7 @@ namespace MvbaMapper
 			{
 				throw new ArgumentNullException("destination");
 			}
-			foreach (var std in GetAccessors(source.GetType(), destination.GetType())
+			foreach (var std in Reflection.GetMatchingProperties(source.GetType(), destination.GetType())
 				.Where(x => x.DestinationPropertyType.IsAssignableFrom(x.SourcePropertyType) ||
 				            x.DestinationPropertyType.IsGenericAssignableFrom(x.SourcePropertyType)))
 			{
