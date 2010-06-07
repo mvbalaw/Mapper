4955d376bb139f67fcfeb61b1e486a183d497148
Clinton Sheppard <csheppard@mvbalaw.com>
2010-06-07 13:30:09 -0500

    separate list and non-list property tests

--- src/MvbaMapperTests/MappingTesterTests.cs
+++ src/MvbaMapperTests/MappingTesterTests.cs
@@ -15,52 +15,22 @@ namespace MvbaMapperTests
 	public class MappingTesterTests
 	{
 		[TestFixture]
-		public class When_asked_to_verify_an_object_mapping
+		public class When_asked_to_verify_an_object_mapping_for_list_fields
 		{
+			private OutputListClassContext _context;
 			private string _expectedDifferenceMessage;
 			private Notification _notification;
 
-			[Test]
-			public void Given_objects_where_only_a_boolean_property_is_different()
+			[SetUp]
+			public void BeforeEachTest()
 			{
-				Test.Given(new MappingTester<OutputClass>())
-					.WithContext(new OutputClassContext())
-					.When(Verify_is_called)
-					.With(A_boolean_property_difference)
-					.Should(Detect_the_difference)
-					.Should(Give_the_name_of_the_property_that_has_the_value_difference)
-					.Verify();
-			}
-
-			[Test]
-			public void Given_objects_where_only_a_DateTime_property_is_different()
-			{
-				Test.Given(new MappingTester<OutputClass>())
-					.WithContext(new OutputClassContext())
-					.When(Verify_is_called)
-					.With(A_DateTime_property_difference)
-					.Should(Detect_the_difference)
-					.Should(Give_the_name_of_the_property_that_has_the_value_difference)
-					.Verify();
-			}
-
-			[Test]
-			public void Given_objects_where_only_a_decimal_property_is_different()
-			{
-				Test.Given(new MappingTester<OutputClass>())
-					.WithContext(new OutputClassContext())
-					.When(Verify_is_called)
-					.With(A_decimal_property_difference)
-					.Should(Detect_the_difference)
-					.Should(Give_the_name_of_the_property_that_has_the_value_difference)
-					.Verify();
+				_context = new OutputListClassContext();
 			}
 
 			[Test]
 			public void Given_objects_where_only_a_List_property_is_different()
 			{
 				Test.Given(new MappingTester<OutputListClass>())
-					.WithContext(new OutputListClassContext())
 					.When(Verify_is_called)
 					.With(A_list_property_difference)
 					.Should(Detect_the_difference)
@@ -68,101 +38,9 @@ namespace MvbaMapperTests
 					.Verify();
 			}
 
-			[Test]
-			public void Given_objects_where_only_a_string_property_is_different()
+			private void A_list_property_difference(MappingTester<OutputListClass> obj)
 			{
-				Test.Given(new MappingTester<OutputClass>())
-					.WithContext(new OutputClassContext())
-					.When(Verify_is_called)
-					.With(A_string_property_difference)
-					.Should(Detect_the_difference)
-					.Should(Give_the_name_of_the_property_that_has_the_value_difference)
-					.Verify();
-			}
-
-			[Test]
-			public void Given_objects_where_only_an_int_property_is_different()
-			{
-				Test.Given(new MappingTester<OutputClass>())
-					.WithContext(new OutputClassContext())
-					.When(Verify_is_called)
-					.With(An_int_property_difference)
-					.Should(Detect_the_difference)
-					.Should(Give_the_name_of_the_property_that_has_the_value_difference)
-					.Verify();
-			}
-
-			private void A_boolean_property_difference(MappingTester<OutputClass> obj, OutputClassContext context)
-			{
-				var source = new ClassFiller<InputClass>().Source;
-				context.Expected = new OutputClass
-					{
-						BooleanProperty = source.BooleanProperty,
-						IntegerProperty = source.IntegerProperty,
-						StringProperty = source.StringProperty,
-						DecimalProperty = source.DecimalProperty,
-						DateTimeProperty = source.DateTimeProperty
-					};
-
-				context.Destination = new OutputClass
-					{
-						BooleanProperty = !context.Expected.BooleanProperty,
-						StringProperty = context.Expected.StringProperty,
-						DecimalProperty = context.Expected.DecimalProperty,
-						DateTimeProperty = context.Expected.DateTimeProperty,
-						IntegerProperty = context.Expected.IntegerProperty
-					};
-
-				_expectedDifferenceMessage = "BooleanProperty";
-			}
-
-			private void A_DateTime_property_difference(MappingTester<OutputClass> obj, OutputClassContext context)
-			{
-				var source = new ClassFiller<InputClass>().Source;
-				context.Expected = new OutputClass
-					{
-						BooleanProperty = source.BooleanProperty,
-						IntegerProperty = source.IntegerProperty,
-						StringProperty = source.StringProperty,
-						DecimalProperty = source.DecimalProperty,
-						DateTimeProperty = source.DateTimeProperty
-					};
-				context.Destination = new OutputClass
-					{
-						BooleanProperty = context.Expected.BooleanProperty,
-						StringProperty = context.Expected.StringProperty,
-						DecimalProperty = context.Expected.DecimalProperty,
-						DateTimeProperty = context.Expected.DateTimeProperty.AddDays(1),
-						IntegerProperty = context.Expected.IntegerProperty
-					};
-				_expectedDifferenceMessage = "DateTimeProperty";
-			}
-
-			private void A_decimal_property_difference(MappingTester<OutputClass> obj, OutputClassContext context)
-			{
-				var source = new ClassFiller<InputClass>().Source;
-				context.Expected = new OutputClass
-					{
-						BooleanProperty = source.BooleanProperty,
-						IntegerProperty = source.IntegerProperty,
-						StringProperty = source.StringProperty,
-						DecimalProperty = source.DecimalProperty,
-						DateTimeProperty = source.DateTimeProperty
-					};
-				context.Destination = new OutputClass
-					{
-						BooleanProperty = context.Expected.BooleanProperty,
-						StringProperty = context.Expected.StringProperty,
-						DecimalProperty = context.Expected.DecimalProperty - 1,
-						DateTimeProperty = context.Expected.DateTimeProperty,
-						IntegerProperty = context.Expected.IntegerProperty
-					};
-				_expectedDifferenceMessage = "DecimalProperty";
-			}
-
-			private void A_list_property_difference(MappingTester<OutputListClass> obj, OutputListClassContext context)
-			{
-				context.Destination = new OutputListClass
+				_context.Destination = new OutputListClass
 					{
 						Items = new List<int>
 							{
@@ -171,7 +49,7 @@ namespace MvbaMapperTests
 								6
 							}
 					};
-				context.Expected = new OutputListClass
+				_context.Expected = new OutputListClass
 					{
 						Items = new List<int>
 							{
@@ -184,10 +62,94 @@ namespace MvbaMapperTests
 				_expectedDifferenceMessage = "Items";
 			}
 
-			private void A_string_property_difference(MappingTester<OutputClass> obj, OutputClassContext context)
+			private void Detect_the_difference()
+			{
+				_notification.IsValid.ShouldBeFalse();
+			}
+
+			private void Give_the_name_of_the_property_that_has_the_value_difference()
+			{
+				_notification.ToString().ShouldBeEqualTo(_expectedDifferenceMessage);
+			}
+
+			private void Verify_is_called(MappingTester<OutputListClass> obj)
+			{
+				_notification = obj.Verify(_context.Destination, _context.Expected);
+			}
+		}
+
+		[TestFixture]
+		public class When_asked_to_verify_an_object_mapping_for_non_list_fields
+		{
+			private OutputClassContext _context;
+			private string _expectedDifferenceMessage;
+			private Notification _notification;
+
+			[SetUp]
+			public void BeforeEachTest()
+			{
+				_context = new OutputClassContext();
+			}
+
+			[Test]
+			public void Given_objects_where_only_a_DateTime_property_is_different()
+			{
+				Test.Given(new MappingTester<OutputClass>())
+					.When(Verify_is_called)
+					.With(A_DateTime_property_difference)
+					.Should(Detect_the_difference)
+					.Should(Give_the_name_of_the_property_that_has_the_value_difference)
+					.Verify();
+			}
+
+			[Test]
+			public void Given_objects_where_only_a_boolean_property_is_different()
+			{
+				Test.Given(new MappingTester<OutputClass>())
+					.When(Verify_is_called)
+					.With(A_boolean_property_difference)
+					.Should(Detect_the_difference)
+					.Should(Give_the_name_of_the_property_that_has_the_value_difference)
+					.Verify();
+			}
+
+			[Test]
+			public void Given_objects_where_only_a_decimal_property_is_different()
+			{
+				Test.Given(new MappingTester<OutputClass>())
+					.When(Verify_is_called)
+					.With(A_decimal_property_difference)
+					.Should(Detect_the_difference)
+					.Should(Give_the_name_of_the_property_that_has_the_value_difference)
+					.Verify();
+			}
+
+			[Test]
+			public void Given_objects_where_only_a_string_property_is_different()
+			{
+				Test.Given(new MappingTester<OutputClass>())
+					.When(Verify_is_called)
+					.With(A_string_property_difference)
+					.Should(Detect_the_difference)
+					.Should(Give_the_name_of_the_property_that_has_the_value_difference)
+					.Verify();
+			}
+
+			[Test]
+			public void Given_objects_where_only_an_int_property_is_different()
+			{
+				Test.Given(new MappingTester<OutputClass>())
+					.When(Verify_is_called)
+					.With(An_int_property_difference)
+					.Should(Detect_the_difference)
+					.Should(Give_the_name_of_the_property_that_has_the_value_difference)
+					.Verify();
+			}
+
+			private void A_DateTime_property_difference(MappingTester<OutputClass> obj)
 			{
 				var source = new ClassFiller<InputClass>().Source;
-				context.Expected = new OutputClass
+				_context.Expected = new OutputClass
 					{
 						BooleanProperty = source.BooleanProperty,
 						IntegerProperty = source.IntegerProperty,
@@ -195,21 +157,89 @@ namespace MvbaMapperTests
 						DecimalProperty = source.DecimalProperty,
 						DateTimeProperty = source.DateTimeProperty
 					};
-				context.Destination = new OutputClass
+				_context.Destination = new OutputClass
 					{
-						BooleanProperty = context.Expected.BooleanProperty,
-						StringProperty = context.Expected.StringProperty.ToUpper(),
-						DecimalProperty = context.Expected.DecimalProperty,
-						DateTimeProperty = context.Expected.DateTimeProperty,
-						IntegerProperty = context.Expected.IntegerProperty
+						BooleanProperty = _context.Expected.BooleanProperty,
+						StringProperty = _context.Expected.StringProperty,
+						DecimalProperty = _context.Expected.DecimalProperty,
+						DateTimeProperty = _context.Expected.DateTimeProperty.AddDays(1),
+						IntegerProperty = _context.Expected.IntegerProperty
+					};
+				_expectedDifferenceMessage = "DateTimeProperty";
+			}
+
+			private void A_boolean_property_difference(MappingTester<OutputClass> obj)
+			{
+				var source = new ClassFiller<InputClass>().Source;
+				_context.Expected = new OutputClass
+					{
+						BooleanProperty = source.BooleanProperty,
+						IntegerProperty = source.IntegerProperty,
+						StringProperty = source.StringProperty,
+						DecimalProperty = source.DecimalProperty,
+						DateTimeProperty = source.DateTimeProperty
+					};
+
+				_context.Destination = new OutputClass
+					{
+						BooleanProperty = !_context.Expected.BooleanProperty,
+						StringProperty = _context.Expected.StringProperty,
+						DecimalProperty = _context.Expected.DecimalProperty,
+						DateTimeProperty = _context.Expected.DateTimeProperty,
+						IntegerProperty = _context.Expected.IntegerProperty
+					};
+
+				_expectedDifferenceMessage = "BooleanProperty";
+			}
+
+			private void A_decimal_property_difference(MappingTester<OutputClass> obj)
+			{
+				var source = new ClassFiller<InputClass>().Source;
+				_context.Expected = new OutputClass
+					{
+						BooleanProperty = source.BooleanProperty,
+						IntegerProperty = source.IntegerProperty,
+						StringProperty = source.StringProperty,
+						DecimalProperty = source.DecimalProperty,
+						DateTimeProperty = source.DateTimeProperty
+					};
+				_context.Destination = new OutputClass
+					{
+						BooleanProperty = _context.Expected.BooleanProperty,
+						StringProperty = _context.Expected.StringProperty,
+						DecimalProperty = _context.Expected.DecimalProperty - 1,
+						DateTimeProperty = _context.Expected.DateTimeProperty,
+						IntegerProperty = _context.Expected.IntegerProperty
+					};
+				_expectedDifferenceMessage = "DecimalProperty";
+			}
+
+			private void A_string_property_difference(MappingTester<OutputClass> obj)
+			{
+				var source = new ClassFiller<InputClass>().Source;
+				_context.Expected = new OutputClass
+					{
+						BooleanProperty = source.BooleanProperty,
+						IntegerProperty = source.IntegerProperty,
+						StringProperty = source.StringProperty,
+						DecimalProperty = source.DecimalProperty,
+						DateTimeProperty = source.DateTimeProperty
+					};
+				_context.Destination = new OutputClass
+					{
+						BooleanProperty = _context.Expected.BooleanProperty,
+						StringProperty = _context.Expected.StringProperty.ToUpper(),
+						DecimalProperty = _context.Expected.DecimalProperty,
+						DateTimeProperty = _context.Expected.DateTimeProperty,
+						IntegerProperty = _context.Expected.IntegerProperty
 					};
 				_expectedDifferenceMessage = "StringProperty";
 			}
 
-			private void An_int_property_difference(MappingTester<OutputClass> obj, OutputClassContext context)
+			private void An_int_property_difference(MappingTester<OutputClass> obj)
 			{
 				var source = new ClassFiller<InputClass>().Source;
-				context.Expected = new OutputClass
+				_context.Expected = new OutputClass
 					{
 						BooleanProperty = source.BooleanProperty,
 						IntegerProperty = source.IntegerProperty,
@@ -217,13 +247,13 @@ namespace MvbaMapperTests
 						DecimalProperty = source.DecimalProperty,
 						DateTimeProperty = source.DateTimeProperty
 					};
-				context.Destination = new OutputClass
+				_context.Destination = new OutputClass
 					{
-						BooleanProperty = context.Expected.BooleanProperty,
-						StringProperty = context.Expected.StringProperty,
-						DecimalProperty = context.Expected.DecimalProperty,
-						DateTimeProperty = context.Expected.DateTimeProperty,
-						IntegerProperty = context.Expected.IntegerProperty - 1
+						BooleanProperty = _context.Expected.BooleanProperty,
+						StringProperty = _context.Expected.StringProperty,
+						DecimalProperty = _context.Expected.DecimalProperty,
+						DateTimeProperty = _context.Expected.DateTimeProperty,
+						IntegerProperty = _context.Expected.IntegerProperty - 1
 					};
 
 				_expectedDifferenceMessage = "IntegerProperty";
@@ -239,14 +269,9 @@ namespace MvbaMapperTests
 				_notification.ToString().ShouldBeEqualTo(_expectedDifferenceMessage);
 			}
 
-			private void Verify_is_called(MappingTester<OutputClass> obj, OutputClassContext context)
+			private void Verify_is_called(MappingTester<OutputClass> obj)
 			{
-				_notification = obj.Verify(context.Destination, context.Expected);
-			}
-
-			private void Verify_is_called(MappingTester<OutputListClass> obj, OutputListClassContext context)
-			{
-				_notification = obj.Verify(context.Destination, context.Expected);
+				_notification = obj.Verify(_context.Destination, _context.Expected);
 			}
 		}
 	}
