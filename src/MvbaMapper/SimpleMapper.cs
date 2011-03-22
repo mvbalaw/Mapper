95b73bf239dc80ec3464fda23bbbfcfcc5a74976
Clinton Sheppard <gar3ts@gmail.com>
2011-03-22 12:16:20 -0500

    add object-type to object-type LruCache to SimpleMapper to reduce reflection calls

--- src/MvbaMapper/SimpleMapper.cs
+++ src/MvbaMapper/SimpleMapper.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+
 using CodeQuery;
+
 using MvbaCore;
 
 namespace MvbaMapper
 {
 	public class SimpleMapper
 	{
+		private static readonly LruCache<string, List<KeyValuePair<string, Action<object, object>>>> FrequentMaps = new LruCache<string, List<KeyValuePair<string, Action<object, object>>>>(50);
+
 		public void Map(object source, object destination)
 		{
 			Map(source, destination, new Expression<Func<object, object>>[0]);
 		}
 
 		public void Map<TDestination>(object source, object destination,
-		                              Expression<Func<TDestination, object>>[] propertiesToIgnore)
+		                              params Expression<Func<TDestination, object>>[] propertiesToIgnore)
 		{
-			var properties = new HashSet<string>(new MapperUtilities().GetPropertyNames(propertiesToIgnore));
 			if (source == null)
 			{
 				return;
@@ -26,13 +29,31 @@ namespace MvbaMapper
 			{
 				throw new ArgumentNullException("destination");
 			}
-			foreach (var std in Reflection.GetMatchingFieldsAndProperties(source.GetType(), destination.GetType())
-				.Where(x => !properties.Contains(x.Name))
-				.Where(x => x.DestinationPropertyType.IsAssignableFrom(x.SourcePropertyType) ||
-				            x.DestinationPropertyType.IsGenericAssignableFrom(x.SourcePropertyType)))
+			var sourceType = source.GetType();
+			var destinationType = destination.GetType();
+			string key = sourceType.FullName + destinationType.FullName;
+			var map = FrequentMaps[key];
+			if (map == null)
 			{
-				var sourceValue = std.GetValueFromSource(source);
-				std.SetValueToDestination(destination, sourceValue);
+				map = Reflection.GetMatchingFieldsAndProperties(sourceType, destinationType)
+					.Where(x => x.DestinationPropertyType.IsAssignableFrom(x.SourcePropertyType) ||
+					            x.DestinationPropertyType.IsGenericAssignableFrom(x.SourcePropertyType))
+					.Select(x => new KeyValuePair<string, Action<object, object>>(x.Name, (s, d) =>
+						{
+							var sourceValue = x.GetValueFromSource(s);
+							x.SetValueToDestination(d, sourceValue);
+						}))
+					.ToList();
+				FrequentMaps.Add(key, map);
+			}
+
+			var properties = propertiesToIgnore.Any() 
+				? new HashSet<string>(new MapperUtilities().GetPropertyNames(propertiesToIgnore)) 
+				: new HashSet<string>();
+			foreach (var action in map
+				.Where(x => !properties.Contains(x.Key)))
+			{
+				action.Value(source, destination);
 			}
 		}
 	}
